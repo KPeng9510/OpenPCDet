71320204783a8c9105abc7ea58c76efa72cdde03
Juncong Fei <jfei@mrtknecht3.mrt.uni-karlsruhe.de>
2021-01-08 17:59:40 +0100

    s

--- tools/train.py
+++ tools/train.py
@@ -77,7 +77,7 @@ def main():
     if args.fix_random_seed:
         common_utils.set_random_seed(666)
 
-    output_dir = Path("/mrtstorage/users/kpeng/train_nuscence/7_1_2021_r") / 'kitti_odo_unet_lightversion_height_4' / cfg.EXP_GROUP_PATH / cfg.TAG / args.extra_tag
+    output_dir = Path("/mrtstorage/users/kpeng/train_nuscence/8_1_2021_r") / 'kitti_odo_unet_lightversion_height_4_sparse' / cfg.EXP_GROUP_PATH / cfg.TAG / args.extra_tag
     ckpt_dir = output_dir / 'ckpt'
     output_dir.mkdir(parents=True, exist_ok=True)
     ckpt_dir.mkdir(parents=True, exist_ok=True)
